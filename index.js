640be88834827f39b4482628fb1bc35a256c9399
ammyy9908 <sb78639@gmail.com>
2022-02-02 10:56:41 +0530

    cors added

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
-
+const { body, validationResult } = require('express-validator');
 const Feedback = require('./models/Feedback');
 const send_mail = require('./utils/sendmail');
 const connectDB = require('./db/db_connection');
 const dotenv = require('dotenv');
+const cors = require('cors');
 dotenv.config();
 
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const port = process.env.PORT || 5000;
@@ -26,11 +28,19 @@ app.get('/', async (req, res) => {
 
     const feedbacks = await Feedback.find({});
     res.send(feedbacks);
-}).post('/feedback',async (req,res)=>{
+}).post('/feedback',body('email').isEmail(),async (req,res)=>{
     const {name,email,message} = req.body;
     const feedback = new Feedback({name,email,message});
     await feedback.save();
     const mailsend = await send_mail(name,message);
+
+
+    const errors = validationResult(req);
+   
+    if (!errors.isEmpty()) {
+      return res.status(200).json({ status:'error',message: errors.array()});
+    }
+
     if(mailsend){
         res.send({
             status: 'success',
